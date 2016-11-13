c725e4616a11e8acc3e56ed6d4ca039dab472a59
areshenk <areshenk@uvic.ca>
2016-11-13 12:54:15 -0800

    Add alternative coarsegrain function

--- R/coarsegrainMultivariate.R
+++ R/coarsegrainMultivariate.R
@@ -1,5 +1,5 @@
-coarsegrainMultivariate = function(mat, eps){
+coarsegrainMultivariate = function(mat, eps, fun = mean, ...){
     # Function coarsegrains a multivariate time series given by a
     # pxn matrix mat (where p is the number of variables)
-    return(t(apply(mat, 1, function(x) coarsegrainUnivariate(x, eps))))
+    return(t(apply(mat, 1, function(x) coarsegrainUnivariate(x, eps, fun, ...))))
 }
--- R/coarsegrainUnivariate.R
+++ R/coarsegrainUnivariate.R
@@ -1,8 +1,8 @@
-coarsegrainUnivariate = function(x, eps){
+coarsegrainUnivariate = function(x, eps, fun = mean, ...){
     # Function coarsegrains a vector x by partitioning it into bins of size eps
     # and computing the average within the bins.
 
     n <- length(x)
     r <- n %% eps
-    return(colMeans(matrix(x[1:(n-r)], nrow = eps)))
+    return(apply(matrix(x[1:(n-r)], nrow = eps), 2, function(i) fun(i, ...)))
 }
